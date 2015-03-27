081d2ed59793dcbf10c1a197d9efc0aae8121b68
Brandon Hafenrichter <bhafenri@Brandons-MacBook-Pro.local>
2015-03-26 22:07:52 -0500

    Initial Commit

---
+++ Who Tweeted It?/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Who Tweeted It?
+//
+//  Created by Brandon Hafenrichter on 3/26/15.
+//  Copyright (c) 2015 Brandon Hafenrichter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Who Tweeted It?/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Who Tweeted It?
+//
+//  Created by Brandon Hafenrichter on 3/26/15.
+//  Copyright (c) 2015 Brandon Hafenrichter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
